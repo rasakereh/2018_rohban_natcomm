13ad0edd6caca7a5457d08de5f589b94b810fabd
rasakereh <rasakereh@manmail.ir>
2021-05-04 16:59:01 +0430

    approximate location info added

---
+++ code/load_sqls_locations_inacc.R
@@ -0,0 +1,228 @@
+library(dplyr)
+library(magrittr)
+library(cytominer)
+library(foreach)
+library(stringr)
+library(readr)
+library(doParallel)
+library(rdist)
+library(infotheo)
+
+dt.saved <- F
+dt.sub.saved <- F
+
+get.flat.dist <- function(df)
+{
+  dists <- pdist(df, metric='euclidean')
+  dists <- dists[upper.tri(dists)]
+  dists[is.na(dists)] <- 0
+  dists
+}
+
+randomized_pdist <- function(df, portion=.1)
+{
+  if(is.null(dim(df)))
+  {
+    df <- df %>% as.data.frame
+  }
+  row.count <- nrow(df)
+  sample.size <- as.integer(row.count * portion)
+  sample.cnt <- as.integer(1 / portion)
+  samples <- replicate(sample.cnt, get.flat.dist(df[sample(row.count, sample.size),])) %>% t
+  apply(samples, 2, mean)
+}
+
+profile.plate.location <- function(pl, project.name, batch.name, n.components = 3000, rand.density = 0.1, correlation, out.path, in.path = NULL, in.type="sqlite", cores = 4, nrm.column = "Metadata_broad_sample", nrm.value = "DMSO", feat.list = NULL) {
+    
+  doParallel::registerDoParallel(cores = cores)
+  
+  sql.path <- ifelse(is.null(in.path), paste0("../backend/", batch.name, "/", pl, "/", pl, ".sqlite"), paste0(in.path, "/", pl, "/", pl, ".sqlite"))
+  out.file <- paste0(out.path, "/", pl, ".csv")
+  
+  if (!file.exists(sql.path)) {
+    system(command = paste0("aws s3 cp 's3://cellpainting-datasets/",
+                            project.name, 
+                            "/workspace/backend/",
+                            batch.name,
+                            "/", 
+                            pl, 
+                            "/", 
+                            pl, 
+                            ".sqlite' ",
+                            sql.path))
+  }
+  
+  norm.path = ifelse(is.null(in.path) , paste0("../input/", pl, "_normalized.csv"), paste0(in.path, "/", pl, "/", pl, "_normalized.csv"))
+  
+  if (!file.exists(norm.path)) {
+    system(command = paste0("aws s3 cp 's3://cellpainting-datasets/",
+                            project.name, 
+                            "/workspace/backend/",
+                            batch.name,
+                            "/", 
+                            pl, 
+                            "/", 
+                            pl, 
+                            "_normalized.csv' ", norm.path))
+  }
+  
+  prf <- readr::read_csv(norm.path)
+  
+  sites.all <- unique(prf$Metadata_Well)
+  
+  variables <- colnames(prf)
+  variables <- variables[which(!str_detect(variables, "Metadata_"))]
+  prf.metadata <- setdiff(colnames(prf), variables)
+  if (!is.null(feat.list)) {
+    variables <- feat.list
+  }
+  
+  sqlite_file <- sql.path
+  db <- DBI::dbConnect(RSQLite::SQLite(), sqlite_file)
+  RSQLite::initExtension(db)
+  
+  image_object_join_columns <- c("TableNumber", "ImageNumber")
+  strata <- c("Image_Metadata_Plate", "Image_Metadata_Well")
+  
+  if(in.type == "sqlite"){
+    image <- dplyr::tbl(src = db, "image") %>%
+      dplyr::select(all_of(c(image_object_join_columns, strata)))
+  }else{
+    root.path = paste0(in.path, "/", pl, "/")
+    image <- read.csv(paste0(root.path, "Image.csv")) %>%
+      dplyr::select(all_of(c(image_object_join_columns, strata)))
+    cells <- read.csv(paste0(root.path, "Cells.csv"))
+    cytoplasm <- read.csv(paste0(root.path, "Cytoplasm.csv"))
+    nuclei <- read.csv(paste0(root.path, "Nuclei.csv"))
+  }
+  
+  image.coll <- image %>%
+    dplyr::select(Image_Metadata_Well, ImageNumber, TableNumber)
+  if(in.type == "sqlite")
+  {
+    image.coll <- image.coll %>% dplyr::collect()
+  }
+  
+  t <- proc.time()
+  
+  profiles <- foreach (sites = sites.all, .combine = rbind) %do% {
+    
+    saveRDS(sites, paste0("../tmp_trad/", sites, ".rds"))  
+    
+    image.sub <- image.coll %>% 
+      dplyr::filter(Image_Metadata_Well == sites) 
+    
+    append_operation_tag <- function(s) stringr::str_c(s, operation, sep = "_")
+    
+    dt.sub <- foreach (i = 1:NROW(image.sub), .combine = rbind) %dopar% {
+      if(in.type == "sqlite"){
+        dbPar <- DBI::dbConnect(RSQLite::SQLite(), sqlite_file)
+        RSQLite::initExtension(dbPar)
+        image <- dplyr::tbl(src = dbPar, "Image") %>%
+          dplyr::select(all_of(c(image_object_join_columns, strata)))
+        cells <- dplyr::tbl(src = dbPar, "Cells")
+        cytoplasm <- dplyr::tbl(src = dbPar, "Cytoplasm")
+        nuclei <- dplyr::tbl(src = dbPar, "Nuclei")
+      }
+
+      cells.sub <- cells %>% 
+        dplyr::filter(ImageNumber == !!image.sub$ImageNumber[i] & TableNumber == !!image.sub$TableNumber[i])
+      
+      cytoplasm.sub <- cytoplasm %>% 
+        dplyr::filter(ImageNumber == !!image.sub$ImageNumber[i] & TableNumber == !!image.sub$TableNumber[i])
+      
+      nuclei.sub <- nuclei %>% 
+        dplyr::filter(ImageNumber == !!image.sub$ImageNumber[i] & TableNumber == !!image.sub$TableNumber[i])
+      
+      if(in.type == "sqlite")
+      {
+        cells.sub <- cells.sub %>% dplyr::collect()
+        cytoplasm.sub <- cytoplasm.sub %>% dplyr::collect()
+        nuclei.sub <- nuclei.sub %>% dplyr::collect()
+        DBI::dbDisconnect(dbPar)
+      }
+      
+      dt <- cells.sub %>%
+        dplyr::inner_join(cytoplasm.sub, by = "ObjectNumber") %>%
+        dplyr::inner_join(nuclei.sub, by = "ObjectNumber") %>%
+        dplyr::inner_join(image.sub, by = image_object_join_columns) 
+      dt
+    }
+    
+    all.variables <- colnames(dt.sub)
+    all.variables <- all.variables[which(str_detect(all.variables, "Cells") | str_detect(all.variables, "Cytoplasm") | str_detect(all.variables, "Nuclei"))]
+    metadata <- setdiff(colnames(dt.sub), all.variables)
+    
+    pos.features <- c('Cells_AreaShape_Center_X', 'Cells_AreaShape_Center_Y')
+    features <- setdiff(all.variables, pos.features)
+    features <- features[seq(1, length(features), 10)]
+    cell.dists <- randomized_pdist(dt.sub[pos.features])
+    discrete.dists <- discretize(cell.dists)
+    location.info <- apply(dt.sub[features], 2, function(col){
+      cell.diff <- randomized_pdist(as.numeric(col))
+      if(correlation == 'pearsonr')
+      {
+        pearson.cor <- cor(cell.dists, cell.diff)
+        if(is.na(pearson.cor))
+        {
+          pearson.cor <- 0
+        }
+        
+        return(pearson.cor)
+      }
+      discrete.diffs <- discretize(cell.diff)
+      mutinformation(discrete.dists, discrete.diffs)
+    }) %>% t %>% as.data.frame
+
+    location.info <- cbind(location.info, dt.sub[1, metadata])
+    
+    location.info <- location.info %>%
+      select(one_of(c(metadata, variables)))
+    
+    # dt.sub[, variables] <- apply(dt.sub[, variables], 2, function(x) as.numeric(x))
+    
+    # if(!(dt.sub.saved))
+    # {
+    #   write.csv(dt.sub, '../dt_sub.csv')
+    #   write.csv(location.info, '../location_info.csv')
+    #   dt.sub.saved <<- TRUE
+    # }
+    
+    profile <- location.info
+    profile <- cbind(profile, data.frame(Metadata_Plate = pl, Metadata_Well = sites))
+  }
+  t2 <- proc.time()
+  
+  cov.variables <- colnames(profiles)
+  cov.variables <- cov.variables[which(str_detect(cov.variables, "Cells_") | str_detect(cov.variables, "Cytoplasm_") | str_detect(cov.variables, "Nuclei_"))]
+  
+  cov.metadata <- setdiff(colnames(profiles), cov.variables)
+  
+  dmso.ids <- prf %>% 
+    dplyr::filter((!!rlang::sym(nrm.column)) == nrm.value) %>%
+    select(Metadata_Plate, Metadata_Well)
+  
+  dt <- profiles[, cov.variables]
+  mn <- apply(dt, 2, function(x) mean(x, na.rm=T))
+  sdv <- apply(dt, 2, function(x) sd(x, na.rm=T))
+  
+  dt.nrm <- scale(dt, center = mn, scale = sdv)
+  
+  profiles.nrm <- cbind(dt.nrm, profiles[, cov.metadata])
+  
+  profiles.nrm.meta <- profiles.nrm[, cov.metadata] %>% 
+    mutate(Metadata_Plate = as.character(Metadata_Plate)) %>%
+    left_join(prf %>% select(one_of(prf.metadata)) %>%
+                mutate(Metadata_Plate = as.character(Metadata_Plate)), by = c("Metadata_Plate", "Metadata_Well"))
+  
+  profiles.nrm <- cbind(profiles.nrm[, cov.variables], profiles.nrm.meta)
+  
+  cov.metadata <- setdiff(colnames(profiles.nrm), cov.variables)
+  
+  readr::write_csv(profiles.nrm, paste0(out.path, "/", pl, "_location.csv"))
+  
+  if(is.null(in.path))
+  {
+    system(paste0(paste0("rm ", sql.path)))
+  }
+}
---
+++ code/profile_loc_inacc.R
@@ -0,0 +1,68 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly = T)
+
+extends <- methods::extends
+
+'profile
+Usage:
+profile_trad -n <project_name> -b <batch_name> -t <input_type> -i <input_dir> -r <output_dir> -a <correlation> -p <plate_number> -o <operation> -l <norm_column> -v <norm_value> -c <no_cores> [-f <feat_list_file>]
+
+Options:
+-h --help                                         Show this screen.
+-n <project_name> --name=<project_name>           Project name on s3.
+-b <batch_name> --batch=<batch_name>              Batch name. 
+-t <input_type> --type=<input_type>               accepts "csv" and "sqlite",
+-i <input_dir> --input=<input_dir>                Path to the directory containing plates,
+-r <output_dir> --result=<output_dir>             Path to the result directory. 
+-p <plate_number> --plate=<plate_number>          Plate number.
+-a <correlation> --correlation=<correlation>      Correlation method; e.g. pearsonr, mutinfo
+-o <operation> --operation=<operation>            Profiling summary function; e.g. mean, mean+sd, etc.
+-f <feat_list_file> --feats=<feat_list_file>      Path to the file containing the list of features. 
+-l <norm_column> --col=<norm_column>              Column name to be used to select samples for normalization.
+-v <norm_value> --value=<norm_value>              Value of the mentioned column which indicates the sample.
+-c <no_cores> --cores=<no_cores>                  Number of cores to be used. 
+' -> doc
+
+opts <- docopt::docopt(doc)
+
+source("load_sqls_locations.R")
+library(readr)
+library(cytominer)
+library(dplyr)
+library(stringr)
+
+pl <- opts[["plate"]]
+proj.name <- opts[["name"]]
+batch.name <- opts[["batch"]]
+input.type <- opts[["type"]]
+input.dir <- opts[["input"]]
+result.dir <- opts[["result"]]
+feat.list <- opts[["feats"]]
+operation <- opts[["operation"]]
+correlation <- opts[["correlation"]]
+col.name <- opts[["col"]]
+col.val <- opts[["value"]]
+cores <- opts[["cores"]]
+
+if (!is.null(feat.list)) {
+  feat.list <- readr::read_csv(feat.list, col_names = F)  
+  feat.list <- unname(unlist(feat.list))
+}
+
+if (!is.null(result.dir)) {
+  outp.dir <- paste0(result.dir, "/", batch.name, "/", pl)
+}else{
+  outp.dir <- paste0("../backend/", batch.name, "/", pl)
+}
+
+if(!dir.exists(outp.dir)) {
+  dir.create(outp.dir, recursive = T)
+}
+
+if(is.null(input.type) || !any(input.type %in% c("sqlite", "csv")))
+{
+  print("Invalid/Unspecified input type, using `sqlite`")
+  input.type <- "sqlite"
+}
+
+profile.plate.location(pl = pl, project.name = proj.name, batch.name = batch.name, nrm.column = col.name, nrm.value = col.val, correlation = correlation, out.path = outp.dir, in.path = input.dir, in.type = input.type, cores = cores, feat.list = feat.list)
