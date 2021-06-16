ace7b5c75ca04d04c0338cd8aae694a7f64cbf3b
rasakereh <rasakereh@manmail.ir>
2021-06-16 07:32:45 +0430

    fusion bug fix 0010

--- code/compare_fusions.R
+++ code/compare_fusions.R
@@ -57,19 +57,20 @@ print('Loading data...')
 # saveRDS(whole.data, 'wholedata.rds')
 whole.data <- readRDS('wholedata.rds')
 
-feats <- whole.data[[1]]$feats
+all.feats <- lapply(whole.data, function(dataset) {dataset$feats})
+feats <- all.feats[[1]]
 meta_feats <- whole.data[[1]]$data %>% colnames %>% setdiff(feats)
 metadata.cols <- lapply(whole.data, function(dataset) dataset$data[,meta_feats])
 sample.names <- metadata.cols[[1]] %>% dplyr::select(Metadata_broad_sample)
 
 print('Imputing missing data...')
-whole.data <- lapply(whole.data, function(dataset){
-  dataset <- dataset$data[,feats]
+whole.data <- lapply(seq_along(whole.data), function(wholedata, name, index){
+  dataset <- wholedata[[index]]$data[,all.feats[[index]]]
   for(i in 1:ncol(dataset)){
     dataset[is.na(dataset[,i]), i] <- mean(dataset[,i], na.rm = TRUE)
   }
   dataset
-})
+}, wholedata=whole.data, name=names(whole.data))
 
 fusion.methods <- c("pseudo-PFA", "MFA", "jNMF", "SNF", "rgcca")
 
