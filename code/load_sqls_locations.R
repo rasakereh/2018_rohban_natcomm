b8e00e598a05956f59b4c195a659910d6fedda6a
rasakereh <rasakereh@manmail.ir>
2021-06-18 21:23:49 +0430

    location error debug 001

--- code/load_sqls_locations.R
+++ code/load_sqls_locations.R
@@ -9,8 +9,7 @@ library(rdist)
 library(infotheo)
 library(kit)
 
-dt.saved <- F
-dt.sub.saved <- F
+bug.detected <- c("BRD-K83508485-001-01-9", "BRD-K83597974-003-05-7", "BRD-K83637872-001-01-7", "BRD-K84175871-003-02-2", "BRD-K84214706-001-02-4", "BRD-K84266862-001-01-6", "BRD-K84421793-001-01-1", "BRD-K84595254-001-03-0", "BRD-K84663978-001-02-3", "BRD-K84709232-001-02-6", "BRD-K85015012-003-01-1", "BRD-K85090592-003-08-7", "BRD-K85104575-001-03-2", "BRD-K85119730-001-06-5", "BRD-K85242180-001-01-1", "BRD-K85266041-304-01-7", "BRD-K85333151-003-04-3", "BRD-K85383046-001-02-5", "BRD-K85555482-001-01-1", "BRD-K85871428-001-01-9", "BRD-K85872723-001-02-7", "BRD-K85880973-001-02-7", "BRD-K85883481-001-02-6", "BRD-K86204871-001-02-2", "BRD-K86301799-001-14-0", "BRD-K86434416-001-02-7", "BRD-K84663978-001-03-1", "BRD-K86509404-001-02-0", "BRD-K86600316-003-01-2", "BRD-K86727142-001-06-6", "BRD-K86873305-236-03-0", "BRD-K86882815-001-01-6", "BRD-K86958018-001-01-9", "BRD-K87048468-003-01-9", "BRD-K87049188-001-03-6", "BRD-K87156652-001-05-1", "BRD-K87194840-001-03-0", "BRD-K87349602-001-01-2", "BRD-K87492696-001-04-1", "BRD-K87510569-001-01-0", "BRD-K87696786-003-01-0", "BRD-K87798455-001-02-6", "BRD-K87817668-001-02-7", "BRD-K87904882-001-02-3", "BRD-K87905482-001-02-4", "BRD-K87990216-001-03-7", "BRD-K87991767-001-02-0", "BRD-K88090157-050-03-3", "BRD-K88172511-310-03-8", "BRD-K88219015-001-02-5", "BRD-K88282786-066-04-9", "BRD-K88358234-003-01-4", "BRD-K88366685-300-03-7", "BRD-K88405679-003-01-5", "BRD-K88429204-001-04-7", "BRD-K88544581-001-01-2", "BRD-K88568253-065-02-1", "BRD-K88611939-001-01-8", "BRD-K88646909-004-03-0", "BRD-K88677950-001-01-3", "BRD-K88682005-001-04-2", "BRD-K88741031-001-01-0", "BRD-K88743730-001-01-0", "BRD-K88759641-003-01-0", "BRD-K88789588-001-03-2", "BRD-K88809146-003-02-1", "BRD-K88849294-001-02-1", "BRD-K88871508-001-01-9", "BRD-K88954184-001-02-2", "BRD-K89046952-001-03-4", "BRD-K89055274-048-03-7", "BRD-K89085489-001-02-6", "BRD-K89125793-001-05-4", "BRD-K89152108-001-03-3", "BRD-K89210380-001-03-8", "BRD-K89225759-001-02-1", "BRD-K89271983-001-01-5", "BRD-K89272762-001-04-4", "BRD-K89274813-001-01-5")
 
 df2numeric <- function(df)
 {
@@ -135,13 +134,19 @@ profile.plate.location <- function(pl, project.name, batch.name, n.components = 
         dplyr::inner_join(cytoplasm.sub, by = "ObjectNumber") %>%
         dplyr::inner_join(nuclei.sub, by = "ObjectNumber") %>%
         dplyr::inner_join(image.sub, by = image_object_join_columns)
-	print(dim(dt))
       dt
     }
     
     all.variables <- colnames(dt.sub)
     all.variables <- all.variables[which(str_detect(all.variables, "Cells") | str_detect(all.variables, "Cytoplasm") | str_detect(all.variables, "Nuclei"))]
     metadata <- setdiff(colnames(dt.sub), all.variables)
+    
+    if(dt.sub[1,"Metadata_broad_sample"] %in% bug.detected)
+    {
+      print('bad well')
+      print(dt.sub[1,"Metadata_broad_sample"])
+      write.csv(paste0('../', dt.sub[1,"Metadata_broad_sample"], '.csv'))
+    }
    
     cell.count = nrow(dt.sub)
     nth.topn = as.integer(cell.count**.5)
